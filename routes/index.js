8159e3aefcab3479d41df7e8ec684afe5584a540
Jotenheim <weston.daniel.a@gmail.com>
2018-11-10 21:45:22 +0000

    Spotify connectivity working, now creates playlist and adds songs relevant to tweet

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request-promise');
-const querystring = require('querystring');
+const Spotify = require('../lib/spotify');
+const terms = require('../bin/terms');
 const fs = require('fs');
 
 const twit = require('twit');
@@ -11,9 +11,8 @@ const Twitter = new twit(twitter_keys);
 const sentiment = require('sentiment');
 const Sentiment = new sentiment();
 
-let terms = ['#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'];
-let stream =  Twitter.stream('statuses/filter', { track: terms, language: 'en' });
-stream.on('tweet', function (tweet) {
+let stream =  Twitter.stream('statuses/filter', { track: ['#', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'y', 'z'], language: 'en' });
+stream.on('tweet', async function (tweet) {
   var mood = (Sentiment.analyze(tweet.text).score + 5) / 10;
   if (mood > 1) {
     mood = 1;
@@ -21,44 +20,22 @@ stream.on('tweet', function (tweet) {
     mood = 0;
   }
 
-  console.log(tweet.text);
-  console.log(`Mood: ${mood}\n`);
+  for (var term in terms) {
+    if (tweet.text.includes(term)) {
+      const songInfo = await Spotify.findNewTrack(term, mood);
+      Spotify.addTrackToPlaylist(term, songInfo);
+    }
+  }
 });
 
-/*const updateStream = function (new_term) {
-  if (terms.includes(new_term)) {
-    terms.splice(terms.indexOf(new_term), 1);
+const updateTerms = async function (term) {
+  if (term in terms) {
+    delete terms[term];
+    fs.writeFileSync('./bin/terms.json', JSON.stringify(terms), 'utf-8');
   } else {
-    terms.push(new_term);
+    await Spotify.addNewPlaylist(term);
   }
-
-  if (terms.length > 0) {
-    let new_stream = ;;
-
-    new_stream.once('connected', function (res) {
-      console.log("NEW STREAM START");
-      console.log("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-      if (stream != null) {
-        stream.stop();
-      }
-
-      stream = new_stream;
-
-      stream.on('tweet', function (tweet) {
-        var mood = (Sentiment.analyze(tweet.text).score + 5) / 10;
-        if (mood > 1) {
-          mood = 1;
-        } else if (mood < 0) {
-          mood = 0;
-        }
-
-        console.log(tweet.text);
-        console.log(terms);
-        console.log(`Mood: ${mood}\n`);
-      });
-    });
-  }
-}*/
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -66,7 +43,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/new_term', function(req, res, next) {
-  updateStream(req.body.new_term);
+  updateTerms(req.body.new_term);
   res.redirect('/');
 })
 
